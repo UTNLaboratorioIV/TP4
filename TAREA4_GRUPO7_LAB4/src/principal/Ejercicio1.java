0e38fd7b63ce7b109b884afd844a23e4b8d51ac6
SantiGiussani <santi.giussani@gmail.com>
2025-04-27 20:43:18 -0300

    Corrección del Tipo de Cierre del Ejercicio 1

--- TAREA4_GRUPO7_LAB4/src/principal/Ejercicio1.java
+++ TAREA4_GRUPO7_LAB4/src/principal/Ejercicio1.java
@@ -39,7 +39,7 @@ public class Ejercicio1 extends JFrame {
 	 */
 	public Ejercicio1() {
 		setTitle("Contactos");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 550, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
